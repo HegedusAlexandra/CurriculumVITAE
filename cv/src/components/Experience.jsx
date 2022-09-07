d140f7ce63a9310b68d99ab23986120adb4ed902
HegedusAlexandra <hexapastry@gmail.com>
2022-09-07 18:35:17 +0200

    begin svg

--- cv/src/components/Experience.jsx
+++ cv/src/components/Experience.jsx
@@ -1,32 +1,31 @@
-import { useEffect,useState } from 'react';
-import './experience.css';
-import me from "./Én.jpg"
+import { useEffect, useState } from "react";
+import "./experience.css";
+import me from "./Én.jpg";
 
 function Experience() {
-
-  const[visib,setVisib] = useState(null)
+  const [visib, setVisib] = useState(null);
 
   useEffect(() => {
-    const handleScroll = event => {
+    const handleScroll = (event) => {
       /* console.log('window.scrollY:', window.scrollY); */
-      setVisib(window.scrollY)
+      setVisib(window.scrollY);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   function reveal() {
     var reveals = document.querySelectorAll(".reveal");
-  
+
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
-  
+
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
@@ -34,18 +33,34 @@ function Experience() {
       }
     }
   }
-  
+
   window.addEventListener("scroll", reveal);
 
   return (
-    <div className='expBack' >
+    <div className="expBack">
       <h1> XPRNC </h1>
-      {visib>50 && 
-      <>              
-        <div className="container reveal">          
-          <img src={me} alt="me"/>
-        </div>        
-      </>}
+      <svg height="3000" width="200">
+        <path
+          stroke-linecap="round"
+          d="M 80 240 s -30 200 50 300 s -50 100 -50 200 t -10 100 50 150 t 10 100 50 150"
+          stroke="#10144A"
+          stroke-width="6"
+          fill="none"
+        />
+
+        <g stroke="black" stroke-width="3" fill="black">
+          <circle id="pointA" cx="76" cy="350" r="10" />
+
+          <circle id="pointC" cx="124" cy="650" r="12" />
+        </g>
+      </svg>
+      {visib > 50 && (
+        <>
+          <div className="container reveal">
+            <img src={me} alt="me" />
+          </div>
+        </>
+      )}
     </div>
   );
 }
