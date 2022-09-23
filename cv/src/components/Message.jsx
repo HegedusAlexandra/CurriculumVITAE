fb8ccde9a616ad2d8587e55011eaa4ed1b4045b1
HegedusAlexandra <hexapastry@gmail.com>
2022-09-23 11:12:51 +0200

    finished contact part

--- cv/src/components/Message.jsx
+++ cv/src/components/Message.jsx
@@ -1,11 +1,127 @@
-/* import './loading.css'; */
+import "./message.css";
 
 function Message() {
-    return (
-      <div className="messageMain">
-        
+  return (
+    <div className="messageMain">
+      <div className="cheerUp"></div>
+      <div className="table">
+      <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"95%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+      <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"115%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <h1 className="contact">E-mail:</h1>
+          <div className="contLineMes">         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"30%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"95%"}}>         
+            <div id="lineMes"></div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <h1 className="profile">hexapastry@gmail.com</h1>
+          <div className="contLineMes" style={{width:"110%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"70%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"60%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <h1 className="contact" >Telephone:</h1>
+          <div className="contLineMes" style={{width:"60%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"75%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <h1 className="profile" >+36 20 237 1798</h1>
+          <div className="contLineMes" style={{width:"80%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"20%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"30%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <h1 className="contact">Facebook:</h1>
+          <div className="contLineMes" style={{width:"50%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record">
+        <a href="https://www.facebook.com/alexandra.hegedus.718/" className="profile">Link to Alexandra's profile</a>
+          <div className="contLineMes" style={{width:"80%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>        
+          <div className="contLineMes" style={{width:"130%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"20%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
+        <div className="record" style={{height:"10px"}}>       
+          <div className="contLineMes" style={{width:"90%"}}>         
+            <div id="lineMes"> </div>
+            <div id="sphereMes"></div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Message;
+    </div>
+  );
+}
+
+export default Message;
